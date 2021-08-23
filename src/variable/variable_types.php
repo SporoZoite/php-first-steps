d4f9719b32f13bb47abc754b6d620c6d51c32d99
SporoZoite <lisavanhooydonk@gmail.com>
2021-08-23 15:43:12 +0200

    variables

---
+++ src/variable/variable_types.php
@@ -0,0 +1,24 @@
+<html>
+  <head><title>Hi!</title></head>
+  <body>
+  <?php
+$name = "Lisa";
+$age = "32";
+$eyes = "green";
+$family = array (
+    0 => "Pierre",
+    1 => "Véronique",
+    2=> "Manon");
+$is_hungry = false;
+
+echo "<p>Hi! my name is $name.</p>";
+echo "<p>I am $age years old.</p>";
+echo "<p>My eyes are $eyes.</p>";
+echo "<p>The first person in my family is $family[0].</p>";
+
+
+?>
+    	
+
+  </body>
+</html>
